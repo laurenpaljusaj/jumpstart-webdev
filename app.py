a6b9e1b474021d0db7fcb117005093831269cd92
laurenpaljusaj <laurenpaljusaj@gmail.com>
2021-08-05 16:49:47 -0400

    added talk template

--- app.py
+++ app.py
@@ -13,7 +13,18 @@ def hello():
 @app.route('/talk/new')
 def new_talk():
     talk_title = talk_mashup_bot.generateTitle()
-    return talk_title
+    return render_template('new_talk.html', title=talk_title)
+
+@app.route('/talk', methods=['POST', 'GET'])
+def talk():
+    if request.method == 'GET':
+        return redirect(url_for('new_talk'))
+
+    talk_title = request.form['form-title']
+    talk_description = request.form['form-description']
+
+    return render_template('talk.html', title=talk_title, description=talk_description)
+
 
 
 if __name__ == '__main__':
